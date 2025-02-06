bcfc3c45956046d6ebacd34e507989d1558bbe1d
Sergei Anisimoff <sergeianisimoff@Sergeis-MacBook-Pro.local>
2025-02-06 16:12:33 +0300

    Create API Request

--- RickAndMortyDouble/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMortyDouble/Controllers/Core/RMCharacterViewController.swift
@@ -14,17 +14,15 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
 		view.backgroundColor = .systemBackground
 		title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            pathComponents: ["1"]
+        )
+        print(request.url ?? "Something wrong here.")
+        
+        RMService.shared.execute(<#T##request: RMRequest##RMRequest#>, expecting: <#T##(Decodable & Encodable).Type#>, completion: <#T##(Result<Decodable & Encodable, any Error>) -> Void#>)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
